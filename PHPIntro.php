b821446e94f39175aa97eb17d12f44f3f4f73d84
HYNER TAYAMBONG <htt7721@students.uc-bcf.edu.ph>
2025-03-12 19:51:59 +0800

    PHP Intro

---
+++ PHPIntro.php
@@ -0,0 +1,9 @@
+<?php
+// Correct way to use <br> as an HTML line break
+$a = 15;
+$b = 7;
+echo "Sum: " . ($a + $b) . "<br>"; // This will print the sum and add a line break
+echo "Difference: " . ($a - $b) . "<br>"; // Prints the difference and a line break
+echo "Product: " . ($a * $b) . "<br>"; // Prints the product and a line break
+echo "Quotient: " . ($a / $b) . "<br>"; // Prints the quotient and a line break
+?>
